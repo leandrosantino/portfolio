11597517a3b3ae85898d196e003f61c77606b60e
leandrosantino <leandrosantino2013@gmail.com>
2024-02-13 20:36:51 -0300

    build

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Leandro Santino | Personal Portfolio';
-// export const SITE_DESCRIPTION = '';
+export const SITE_DESCRIPTION = '';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
