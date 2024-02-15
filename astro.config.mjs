7560f5c91690613a8835b48cee60979acb24d02a
leandrosantino <leandrosantino2013@gmail.com>
2024-02-14 21:40:53 -0300

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,16 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://leandrosantino.github.io',
-  dase: '/portifolio',
-  server: {
-    host: true
-  },
-  integrations: [mdx(), sitemap(), tailwind()]
+  // site: 'https://leandrosantino.github.io',
+  // dase: '/portifolio',
+  // server: {
+  //   host: true
+  // },
+  integrations: [mdx(), sitemap(), tailwind()],
+  output: "server",
+  adapter: netlify()
 });
